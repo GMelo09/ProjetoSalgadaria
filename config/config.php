f91fd47d3811ba9ffc4b816faf0b7e105311501b
GMelo09 <gustavo.m.lima09@outlook.com>
2026-03-31 21:12:38 -0300

    adiciona gitignore

--- config/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Configurações do banco de dados
-
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'docesalgado');
-define('DB_USER', 'root'); // usuário do banco
-define('DB_PASS', '');     // senha do banco
-
